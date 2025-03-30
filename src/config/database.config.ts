44ecbba1349a9ebb6fe521b1eb68248d4c0f5db5
kepengqi <kepengqi@gmail.com>
2025-03-30 16:22:25 +0800

    Refactor database config to separate file

---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+
+export const getDatabaseConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => {
+  const dbPassword = configService.get('DB_PASSWORD', 'password');
+  return {
+    type: configService.get<'mysql'>('DB_TYPE', 'mysql'),
+    host: configService.get('DB_HOST', 'localhost'),
+    port: configService.get<number>('DB_PORT', 3306),
+    username: configService.get('DB_USERNAME', 'root'),
+    password: typeof dbPassword === 'function' ? dbPassword() : dbPassword,
+    database: configService.get('DB_NAME', 'yideng'),
+    entities: [User],
+    synchronize: configService.get<boolean>('DB_SYNC', false),
+    autoLoadEntities: true,
+  } as TypeOrmModuleOptions;
+}; 
