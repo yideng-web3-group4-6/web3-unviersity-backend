c6e3f94b729f8afbf97b4c7e629b0396849ab2b2
user-fairX <bobfreda.66@gmail.com>
2025-04-01 10:43:08 +0800

    fix: add upload video confirm api

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        // secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '1d' },
       }),
       inject: [ConfigService],
