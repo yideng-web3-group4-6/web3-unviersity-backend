58ce52a45c09330a0d5621f1ca4bf2731ab5e474
yxnne <506524339@qq.com>
2025-04-16 22:40:15 +0800

    fix(article): jwt error

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Article } from './entities/article.entity';
 import { User } from '../user/entities/user.entity';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article, User])],
+  imports: [
+    TypeOrmModule.forFeature([Article, User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [ArticleController],
   providers: [ArticleService],
   exports: [ArticleService],
